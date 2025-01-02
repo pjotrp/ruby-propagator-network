df2dff037bf9ce7e562034f8f5f30fcd23f214fb
Pjotr Prins <pjotr.public01@thebird.nl>
2025-01-02 02:09:45 -0600

    Add another computation

--- propagator.rb
+++ propagator.rb
@@ -28,7 +28,9 @@ class Propagator
 
     if @state == :waiting
       # Check inputs
+      p self
       @inputs.each do | input |
+        p input.value
         return false if input.value == :nothing
       end
       @state = :compute
@@ -41,6 +43,8 @@ class Propagator
         return true
       end
     end
+
+
     false
   end
 end
@@ -58,57 +62,96 @@ class P_Plus < Propagator
   end
 end
 
+class P_Multiply < Propagator
+  def connect(pn, a, b, c)
+    pn.append self
+    @inputs = [ a, b ]
+    @output = c
+  end
+
+  def f
+    @output.value = inputs[0].value * inputs[1].value
+    true
+  end
+end
+
 def run pn
   # Run the propnet in a round robin fashion
   done = true
-  try = 3 # max runs
+  try = 10 # max runs
   begin
-    done = true
+    result = []
     # Try all propagators until they all return true
     pn.each do | propagator |
-      try -= 1
-      done &&= propagator.run
-      p [try, propagator, done]
+      try = try-1
+      result.push propagator.run
+      p [try, propagator]
     end
+    p result
+    done = !result.index(false)
   end until done or try <= 0
 end
 
+# Create the propnet. Note that the order of creating cells and propagators should not matter!
 propnet = []
 
 a = Cell.new()
 b = Cell.new()
 c = Cell.new()
-
-p_plus = P_Plus.new()
-
-a.value = 2
-b.value = 3
-
-p_plus.connect(propnet, a, b, c)
-
 d = Cell.new()
 e = Cell.new()
-
-d.value = 5
+f = Cell.new()
 
 p_plus2 = P_Plus.new()
 p_plus2.connect(propnet, c, d, e)
 
+p_plus = P_Plus.new()
+p_plus.connect(propnet, a, b, c)
+
+p_multiply = P_Multiply.new()
+p_multiply.connect(propnet, e, d, f)
+
+a.value = 2
+b.value = 3
+d.value = 5
+
 run(propnet)
 
+# Inspect results
 p propnet
 p c.value
 p e.value
+p f.value
 
 =begin
 
-Results:
+Resulting in:
 
 gaeta:~/iwrk/opensource/ruby/propagator$ ./propagator.rb
-[2, #<P_Plus:0x00007fd57b838a08 @state=:done, @inputs=[#<Cell:0x00007fd57b839160 @value=2>, #<Cell:0x00007fd57b838d50 @value=3>], @output=#<Cell:0x00007fd57b838c88 @value=5>>, true]
-[1, #<P_Plus:0x00007fd57b838760 @state=:done, @inputs=[#<Cell:0x00007fd57b838c88 @value=5>, #<Cell:0x00007fd57b838850 @value=5>], @output=#<Cell:0x00007fd57b8387b0 @value=10>>, true]
-[#<P_Plus:0x00007fd57b838a08 @state=:done, @inputs=[#<Cell:0x00007fd57b839160 @value=2>, #<Cell:0x00007fd57b838d50 @value=3>], @output=#<Cell:0x00007fd57b838c88 @value=5>>, #<P_Plus:0x00007fd57b838760 @state=:done, @inputs=[#<Cell:0x00007fd57b838c88 @value=5>, #<Cell:0x00007fd57b838850 @value=5>], @output=#<Cell:0x00007fd57b8387b0 @value=10>>]
+#<P_Plus:0x00007fccf9117948 @state=:waiting, @inputs=[#<Cell:0x00007fccf9117fd8 @value=:nothing>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117bf0 @value=:nothing>>
+:nothing
+[9, #<P_Plus:0x00007fccf9117948 @state=:waiting, @inputs=[#<Cell:0x00007fccf9117fd8 @value=:nothing>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117bf0 @value=:nothing>>]
+#<P_Plus:0x00007fccf91175b0 @state=:waiting, @inputs=[#<Cell:0x00007fccf9110080 @value=2>, #<Cell:0x00007fccf9110008 @value=3>], @output=#<Cell:0x00007fccf9117fd8 @value=:nothing>>
+2
+3
+[8, #<P_Plus:0x00007fccf91175b0 @state=:done, @inputs=[#<Cell:0x00007fccf9110080 @value=2>, #<Cell:0x00007fccf9110008 @value=3>], @output=#<Cell:0x00007fccf9117fd8 @value=5>>]
+#<P_Multiply:0x00007fccf9117448 @state=:waiting, @inputs=[#<Cell:0x00007fccf9117bf0 @value=:nothing>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117b00 @value=:nothing>>
+:nothing
+[7, #<P_Multiply:0x00007fccf9117448 @state=:waiting, @inputs=[#<Cell:0x00007fccf9117bf0 @value=:nothing>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117b00 @value=:nothing>>]
+[false, true, false]
+#<P_Plus:0x00007fccf9117948 @state=:waiting, @inputs=[#<Cell:0x00007fccf9117fd8 @value=5>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117bf0 @value=:nothing>>
+5
+5
+[6, #<P_Plus:0x00007fccf9117948 @state=:done, @inputs=[#<Cell:0x00007fccf9117fd8 @value=5>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117bf0 @value=10>>]
+[5, #<P_Plus:0x00007fccf91175b0 @state=:done, @inputs=[#<Cell:0x00007fccf9110080 @value=2>, #<Cell:0x00007fccf9110008 @value=3>], @output=#<Cell:0x00007fccf9117fd8 @value=5>>]
+#<P_Multiply:0x00007fccf9117448 @state=:waiting, @inputs=[#<Cell:0x00007fccf9117bf0 @value=10>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117b00 @value=:nothing>>
+10
+5
+[4, #<P_Multiply:0x00007fccf9117448 @state=:done, @inputs=[#<Cell:0x00007fccf9117bf0 @value=10>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117b00 @value=50>>]
+[true, true, true]
+[#<P_Plus:0x00007fccf9117948 @state=:done, @inputs=[#<Cell:0x00007fccf9117fd8 @value=5>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117bf0 @value=10>>, #<P_Plus:0x00007fccf91175b0 @state=:done, @inputs=[#<Cell:0x00007fccf9110080 @value=2>, #<Cell:0x00007fccf9110008 @value=3>], @output=#<Cell:0x00007fccf9117fd8 @value=5>>, #<P_Multiply:0x00007fccf9117448 @state=:done, @inputs=[#<Cell:0x00007fccf9117bf0 @value=10>, #<Cell:0x00007fccf9117f88 @value=5>], @output=#<Cell:0x00007fccf9117b00 @value=50>>]
 5
 10
+50
 
 =end
